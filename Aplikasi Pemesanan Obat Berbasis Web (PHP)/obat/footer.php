b437167ec0453ea3f8c6ae5de0e3732a2ecd6e10
RioJuan <rio.juan6542@student.unri.ac.id>
2021-09-03 13:45:09 -0700

    Memasukkan Aplikasi ke GIT

---
+++ Aplikasi Pemesanan Obat Berbasis Web (PHP)/obat/footer.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            Copyright &COPY; <?= date('Y') ?> Developer Apotek Tanjungbalai |
+            Repost by <a href="https://stokcoding.com/" title="StokCoding.com" target="_blank">StokCoding.com</a>
+        </div>
+    </div>
+</div>
